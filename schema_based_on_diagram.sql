0b6be3b4d84eb0031c3af195fe1e5aba0a97d0c3
Zafron047 <117600135+Zafron047@users.noreply.github.com>
2023-08-24 22:23:19 +0600

    Create table for medical_histories

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -4,3 +4,12 @@ CREATE TABLE patients (
   name VARCHAR(255),
   date_of_birth DATE
 );
+
+-- Create a table for medical_histories
+CREATE TABLE medical_histories (
+  id SERIAL PRIMARY KEY,
+  admitted_at timestamp,
+  patient_id INT,
+  status VARCHAR(100),
+  CONSTRAINT patient_id_fk FOREIGN KEY(patient_id) REFERENCES patients(id)
+);
