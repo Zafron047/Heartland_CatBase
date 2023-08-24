3eaf4ad18eff7e9a4d776c51d9e4f8b2c9f8dbf3
Zafron047 <117600135+Zafron047@users.noreply.github.com>
2023-08-24 19:53:31 +0600

    Populate database with data and add explain analyze queries to test performance

---
+++ data.sql
@@ -0,0 +1,174 @@
+-- Day 1
+
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts)
+VALUES
+('Agumon', '2020-02-03', 10.23, TRUE, 0),
+('Gabumon', '2018-11-15', 8, TRUE, 2),
+('Pikachu', '2021-01-07', 15.04, FALSE, 1),
+('Devimon', '2017-05-12', 11, TRUE, 5);
+
+-- Day 2
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts)
+VALUES
+('Charmander', '2020-02-08', -11, FALSE, 0),
+('Plantmon', '2021-11-15', -5.7, TRUE, 2),
+('Squirtle', '1993-04-02', -12.13, FALSE, 3),
+('Angemon', '2005-06-12', -45, TRUE, 1),
+('Boarmon', '2005-06-07', 20.4, TRUE, 7),
+('Blossom', '1998-10-13', 17, TRUE, 3),
+('Ditto', '2022-05-14', 22, TRUE, 4);
+
+-- Day 3
+-- Insert the following data into the owners table:
+--   Sam Smith 34 years old.
+--   Jennifer Orwell 19 years old.
+--   Bob 45 years old.
+--   Melody Pond 77 years old.
+--   Dean Winchester 14 years old.
+--   Jodie Whittaker 38 years old.
+INSERT INTO owners(full_name, age)
+VALUES
+('Sam Smith', 34),
+('Jennifer Orwell', 19),
+('Bob', 45),
+('Melody Pond', 77),
+('Dean Winchester', 14),
+('Jodie Whittaker', 38);
+-- DONE
+
+-- Insert the following data into the species table:
+--   Pokemon
+--   Digimon
+INSERT INTO species(name)
+VALUES
+('Pokemon'),
+('Digimon');
+-- DONE
+
+-- Modify your inserted animals so it includes the species_id value:
+--   If the name ends in "mon" it will be Digimon
+UPDATE animals 
+SET species_id = (SELECT id FROM species WHERE name = 'Digimon') 
+WHERE name LIKE '%mon';
+-- DONE
+
+--   All other animals are Pokemon
+UPDATE animals 
+SET species_id = (SELECT id FROM species WHERE name = 'Pokemon') 
+WHERE species_id IS NULL;
+-- DONE
+
+-- Modify your inserted animals to include owner information (owner_id):
+--   Sam Smith owns Agumon.
+UPDATE animals
+SET owner_id = (SELECT id FROM owners WHERE full_name = 'Sam Smith')
+WHERE name = 'Agumon';
+-- DONE
+
+--   Jennifer Orwell owns Gabumon and Pikachu.
+UPDATE animals
+SET owner_id = (SELECT id FROM owners WHERE full_name = 'Jennifer Orwell')
+WHERE name IN ('Gabumon', 'Pikachu');
+-- DONE
+
+--   Bob owns Devimon and Plantmon.
+UPDATE animals
+SET owner_id = (SELECT id FROM owners WHERE full_name = 'Bob')
+WHERE name IN ('Devimon', 'Plantmon');
+-- DONE
+
+--   Melody Pond owns Charmander, Squirtle, and Blossom.
+UPDATE animals
+SET owner_id = (SELECT id FROM owners WHERE full_name = 'Melody Pond')
+WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
+-- DONE
+
+--   Dean Winchester owns Angemon and Boarmon.
+UPDATE animals
+SET owner_id = (SELECT id FROM owners WHERE full_name = 'Dean Winchester')
+WHERE name IN ('Angemon', 'Boarmon');
+-- DONE
+
+-- Day 4
+-- `vets`: an animal can visit multiple vets and one vet can be visited by multiple animals. Create a "join table" called `visits` to handle this relationship, it should also keep track of the date of the visit.
+-- - Insert the following data for vets:
+--   - Vet William Tatcher is 45 years old and graduated Apr 23rd, 2000.
+--   - Vet Maisy Smith is 26 years old and graduated Jan 17th, 2019.
+--   - Vet Stephanie Mendez is 64 years old and graduated May 4th, 1981.
+--   - Vet Jack Harkness is 38 years old and graduated Jun 8th, 2008.
+INSERT INTO vets ( name, age, date_of_graduation)
+VALUES ('William Tatcher', 45, '2000-04-23'),
+('Maisy Smith', 26, '2019-01-17'),
+('Stephanie Mendez', 64, '1981-05-04'),
+('Jack Harkness', 38, '2000-04-23');
+
+
+-- - Insert the following data for specializations:
+--   - Vet William Tatcher is specialized in Pokemon.
+--   - Vet Stephanie Mendez is specialized in Digimon and Pokemon.
+--   - Vet Jack Harkness is specialized in Digimon.
+INSERT INTO specializations (vet_id, species_id)
+VALUES 
+(1, 1),
+(3, 1),
+(3, 2),
+(4, 2);
+-- DONE
+
+-- - Insert the following data for visits:
+--   - Agumon visited William Tatcher on May 24th, 2020.
+--   - Agumon visited Stephanie Mendez on Jul 22th, 2020.
+--   - Gabumon visited Jack Harkness on Feb 2nd, 2021.
+--   - Pikachu visited Maisy Smith on Jan 5th, 2020.
+--   - Pikachu visited Maisy Smith on Mar 8th, 2020.
+--   - Pikachu visited Maisy Smith on May 14th, 2020.
+--   - Devimon visited Stephanie Mendez on May 4th, 2021.
+--   - Charmander visited Jack Harkness on Feb 24th, 2021.
+--   - Plantmon visited Maisy Smith on Dec 21st, 2019.
+--   - Plantmon visited William Tatcher on Aug 10th, 2020.
+--   - Plantmon visited Maisy Smith on Apr 7th, 2021.
+--   - Squirtle visited Stephanie Mendez on Sep 29th, 2019.
+--   - Angemon visited Jack Harkness on Oct 3rd, 2020.
+--   - Angemon visited Jack Harkness on Nov 4th, 2020.
+--   - Boarmon visited Maisy Smith on Jan 24th, 2019.
+--   - Boarmon visited Maisy Smith on May 15th, 2019.
+--   - Boarmon visited Maisy Smith on Feb 27th, 2020.
+--   - Boarmon visited Maisy Smith on Aug 3rd, 2020.
+--   - Blossom visited Stephanie Mendez on May 24th, 2020.
+--   - Blossom visited William Tatcher on Jan 11th, 2021.
+INSERT INTO visits ( animals_id, vet_id, visit_date)
+VALUES
+(1, 1, '2020-05-24'),
+(1, 3, '2020-07-22'),
+(2, 4, '2021-02-02'),
+(3, 2, '2020-01-05'),
+(3, 2, '2020-03-08'),
+(3, 2, '2020-05-14'),
+(4, 3, '2021-05-04'),
+(5, 4, '2021-02-24'),
+(6, 2, '2019-12-21'),
+(6, 1, '2020-08-10'),
+(6, 2, '2021-04-07'),
+(7, 3, '2019-09-29'),
+(8, 4, '2020-10-03'),
+(8, 4, '2020-11-04'),
+(9, 2, '2019-01-24'),
+(9, 2, '2019-05-15'),
+(9, 2, '2020-02-27'),
+(9, 2, '2020-08-03'),
+(10, 3, '2020-05-24'),
+(10, 1, '2021-01-11');
+
+-- Day 5
+
+-- This will add 3.594.280 visits considering you have 10 animals, 4 vets, and it will use around ~87.000 timestamps (~4min approx.)
+INSERT INTO visits (animals_id, vet_id, visit_date)
+SELECT * FROM (SELECT id FROM animals) animal_ids,
+(SELECT id FROM vets) vets_ids,
+generate_series('1980-01-01'::timestamp,
+'2021-01-01', '4 hours') visit_timestamp;
+
+-- This will add 2.500.000 owners with full_name = 'Owner <X>' and email = 'owner_<X>@email.com' (~2min approx.)
+insert into owners (full_name, email) 
+select 'Owner ' || generate_series(1,2500000), 
+'owner_' || generate_series(1,2500000) || '@mail.com';
---
+++ querries.sql
@@ -0,0 +1,300 @@
+-- Day 1 ---------- Begin -------------
+
+-- Find all animals whose name ends in "mon".
+SELECT * from animals
+WHERE name LIKE '%mon';
+
+-- List the name of all animals born between 2016 and 2019.
+SELECT * FROM animals
+WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
+
+-- List the name of all animals that are neutered and have less than 3 escape attempts.
+SELECT * FROM animals
+WHERE neutered = TRUE AND escape_attempts < 3;
+
+-- List the date of birth of all animals named either "Agumon" or "Pikachu".
+SELECT date_of_birth FROM animals
+WHERE name IN ('Agumon', 'Pikachu');
+
+-- List name and escape attempts of animals that weigh more than 10.5kg
+SELECT name, escape_attempts FROM animals
+WHERE weight_kg > 10.5;
+
+-- Find all animals that are neutered.
+SELECT * FROM animals
+WHERE neutered = TRUE;
+
+-- Find all animals not named Gabumon.
+SELECT * FROM animals
+WHERE name <> 'Gabumon';
+
+-- Find all animals with a weight between 10.4kg and 17.3kg (including the animals with the weights that equals precisely 10.4kg or 17.3kg)
+SELECT * FROM animals
+WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+-- Day 1 --------- XXxxXX ------------
+
+-- Day 2 ---------- Begin -------------
+
+-- Task 1 =======>>
+-- Inside a transaction update the animals table by setting the species column to unspecified. Verify 
+-- that change was made. Then roll back the change and verify that the species columns went back to the 
+-- state before the transaction.
+
+-- Inside a Transaction:
+BEGIN;
+
+-- Update animals table by setting Species Column to unspecified.
+UPDATE animals
+SET species = 'unspecified';
+
+-- Verify that change was made.
+SELECT species FROM animals;
+
+-- Roll Back the Transaction
+ROLLBACK;
+
+-- Verify the Rollback
+SELECT species FROM animals;
+
+-- Task 2 =======>>
+-- Start a transaction
+BEGIN;
+
+-- Update the animals table by setting the species column to digimon for all animals that have a name ending in mon.
+UPDATE animals
+SET species = 'digimon'
+WHERE name LIKE '%mon';
+
+-- Update the animals table by setting the species column to pokemon for all animals that don't have species already set.
+UPDATE animals
+SET species = 'pokemon'
+WHERE species IS NULL;
+
+-- Verify that changes were made.
+SELECT species FROM animals;
+
+-- Commit the transaction.
+COMMIT;
+
+-- Verify that changes persist after commit.
+SELECT species FROM animals;
+
+-- Task 3 =======>>
+-- Inside a transaction delete all records in the animals table, then roll back the transaction.
+
+-- Inside a transaction
+BEGIN;
+
+-- delete all records
+DELETE FROM animals;
+
+-- then roll back the transaction
+ROLLBACK;
+
+-- After the rollback verify if all records in the animals table still exists.
+SELECT COUNT(*) FROM animals;
+
+-- Task 4 =======>>
+-- Inside a transaction:
+BEGIN;
+
+-- 1# Delete all animals born after Jan 1st, 2022
+DELETE FROM animals
+WHERE date_of_birth > '2022-01-01';
+
+-- 2# Create a savepoint for the transaction
+SAVEPOINT save_01;
+
+-- 3# Update all animals' weight to be their weight multiplied by -1
+UPDATE animals
+SET weight_kg = weight_kg * -1;
+
+-- 4# Rollback to the savepoint
+ROLLBACK TO save_01;
+
+-- 5# Update all animals' weights that are negative to be their weight multiplied by -1
+UPDATE animals
+SET weight_kg = weight_kg * -1
+WHERE weight_kg < 0;
+
+-- 6# Commit transaction
+COMMIT;
+
+-- Task 5 =======>>
+-- Write queries to answer the following questions:
+-- 1# How many animals are there?
+SELECT COUNT(*) AS nums_of_animals FROM animals;
+
+-- 2# How many animals have never tried to escape?
+SELECT COUNT(*) AS zero_escape FROM animals
+WHERE escape_attempts = 0;
+
+-- 3# What is the average weight of animals?
+SELECT AVG(weight_kg) AS avg_weight FROM animals;
+
+-- 4# Who escapes the most, neutered or not neutered animals?
+SELECT neutered FROM animals
+GROUP BY neutered
+ORDER BY SUM(escape_attempts) DESC
+LIMIT 1;
+
+-- 5# What is the minimum and maximum weight of each type of animal?
+SELECT Species, MIN(weight_kg), MAX(weight_kg) FROM animals
+GROUP BY Species;
+
+-- 6# What is the average number of escape attempts per animal type of those born between 1990 and 2000?
+SELECT Species, AVG(escape_attempts) FROM animals
+WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31'
+GROUP BY Species;
+
+-- Day 3
+-- Write queries (using JOIN) to answer the following questions:
+--   What animals belong to Melody Pond?
+SELECT animals.id AS id, name AS name, owner_id AS owner_id, full_name AS owner_name
+FROM animals
+INNER JOIN owners ON animals.owner_id = owners.id
+WHERE full_name = 'Melody Pond';
+-- DONE
+
+--   List of all animals that are pokemon (their type is Pokemon).
+SELECT animals.id AS id, animals.name as animal_name, species_id AS specie_id, species.name AS specie_type
+FROM animals
+INNER JOIN species ON animals.species_id = species.id 
+WHERE species.name = 'Pokemon';
+-- DONE
+
+--   List all owners and their animals, remember to include those that don't own any animal.
+SELECT owners.id AS owner_id, full_name AS owner_name, animals.id AS animal_id, name as animal_name
+FROM animals
+RIGHT JOIN owners ON animals.owner_id = owners.id;
+-- DONE
+
+--   How many animals are there per species?
+SELECT species.name As specie, COUNT(species.name) AS count
+FROM animals
+INNER JOIN species ON animals.species_id = species.id
+GROUP BY species.name;
+-- DONE
+
+--   List all Digimon owned by Jennifer Orwell.
+SELECT animals.name, owners.full_name
+FROM animals
+INNER JOIN owners ON animals.owner_id = owners.id
+INNER JOIN species ON animals.species_id = species.id
+WHERE owners.full_name = 'Jennifer Orwell';
+-- DONE
+
+--   List all animals owned by Dean Winchester that haven't tried to escape.
+SELECT animals.name AS animal_no_escape, owners.full_name AS owns_by
+FROM animals
+INNER JOIN owners ON animals.owner_id = owners.id
+WHERE escape_attempts = 0 AND owners.full_name = 'Dean Winchester';
+-- DONE
+
+--   Who owns the most animals?
+SELECT owners.full_name AS owned_by, COUNT(owners.full_name) AS total
+FROM animals
+INNER JOIN owners ON animals.owner_id = owners.id
+GROUP BY owners.full_name
+ORDER BY total DESC;
+-- DONE
+
+-- Day 4
+-- - Write queries to answer the following:
+--   - Who was the last animal seen by William Tatcher?
+SELECT animals.name AS last_visited_animal 
+FROM visits 
+JOIN vets ON visits.vet_id = vets.id 
+JOIN animals ON visits.animals_id = animals.id 
+WHERE vets.name = 'William Tatcher' 
+ORDER BY visit_date DESC 
+LIMIT 1;
+-- DONE
+
+--   - How many different animals did Stephanie Mendez see?
+SELECT COUNT(animals.name) AS visit_different_animals, vets.name AS doctor_name 
+FROM animals 
+JOIN visits ON visits.animals_id = animals.id 
+JOIN vets ON visits.vet_id = vets.id 
+WHERE vets.name = 'Stephanie Mendez' 
+GROUP BY vets.name;
+-- DONE
+
+--   - List all vets and their specialties, including vets with no specialties.
+SELECT vets.name AS vet_name, species.name AS specialized_in
+FROM specializations
+FULL JOIN vets ON specializations.vet_id = vets.id
+FULL JOIN species ON specializations.species_id = species.id;
+-- DONE
+
+--   - List all animals that visited Stephanie Mendez between April 1st and August 30th, 2020.
+SELECT animals.name AS animals_name, vets.name AS vets_name 
+FROM visits 
+JOIN vets ON visits.vet_id = vets.id 
+JOIN animals ON visits.animals_id = animals.id 
+WHERE vets.name = 'Stephanie Mendez' AND visit_date BETWEEN '2020-04-01' AND '2020-08-31';
+-- DONE
+
+--   - What animal has the most visits to vets?
+SELECT COUNT(animals.name) AS total_visits, animals.name AS animal 
+FROM visits 
+JOIN animals ON visits.animals_id = animals.id 
+GROUP BY animals.name 
+ORDER BY total_visits DESC 
+LIMIT 1;
+-- DONE
+
+--   - Who was Maisy Smith's first visit?
+SELECT MIN(visit_date) AS first_visited, animals.name AS animal 
+FROM visits 
+JOIN vets ON visits.vet_id = vets.id 
+JOIN animals ON visits.animals_id = animals.id 
+WHERE vets.name = 'Maisy Smith' 
+GROUP BY animals.name 
+ORDER BY first_visited ASC 
+LIMIT 1;
+-- DONE
+
+--   - Details for most recent visit: animal information, vet information, and date of visit.
+SELECT MAX(visit_date) AS recently_visited, animals.name AS animal, vets.name AS vet_name 
+FROM visits 
+JOIN animals ON visits.animals_id = animals.id 
+JOIN vets ON visits.vet_id = vets.id 
+GROUP BY animals.name, vets.name 
+ORDER BY recently_visited DESC 
+LIMIT 1;
+-- DONE
+
+--   - How many visits were with a vet that did not specialize in that animal's species?
+SELECT COUNT(visit_date) AS non_specialized_visits 
+FROM specializations 
+JOIN vets ON specializations.vet_id = vets.id 
+JOIN visits ON specializations.vet_id = visits.vet_id 
+WHERE species_id IS NULL;
+-- DONE
+
+--   - What specialty should Maisy Smith consider getting? Look for the species she gets the most.
+SELECT species.name AS frequent_visit_to_specific_specialist, vets.name AS doctor_name 
+FROM animals
+JOIN visits ON animals.id = visits.animals_id 
+JOIN vets ON visits.vet_id = vets.id 
+JOIN species ON animals.species_id = species.id 
+WHERE vets.name = 'Maisy Smith' 
+GROUP BY species.name, vets.name 
+ORDER BY COUNT(species.name) DESC 
+LIMIT 1;
+-- DONE
+
+-- Day 5
+-- The following queries are taking too much time (1 sec = 1000ms can be considered as too much time for database query).
+-- Try them on your machine to confirm it:
+
+EXPLAIN ANALYZE SELECT COUNT(*) FROM visits
+WHERE animals_id = 4;
+
+EXPLAIN ANALYZE SELECT * FROM visits 
+WHERE vet_id = 2;
+
+EXPLAIN ANALYZE SELECT * FROM owners 
+WHERE email = 'owner_18327@mail.com';
