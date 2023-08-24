a3773644df3a62abbced1cd83eff966ec94c69d8
Zafron047 <117600135+Zafron047@users.noreply.github.com>
2023-08-24 22:24:03 +0600

    create table for invoices

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -20,3 +20,13 @@ CREATE TABLE treatments (
   type VARCHAR(255),
   name VARCHAR(255)
 );
+
+-- Create a table for invoices
+CREATE TABLE invoices (
+  id SERIAL PRIMARY KEY,
+  total_amount DECIMAL(10, 2),
+  generated_at timestamp,
+  payed_at timestamp,
+  medical_history_id INT,
+  CONSTRAINT medical_history_id_fk FOREIGN KEY(medical_history_id) REFERENCES medical_history(id)
+);
